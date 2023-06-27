cc76fd07b4b8324078aa5a296cf718429f7fe6b6
xufeixiang <xufeixiang@tenwit.com>
2023-06-27 12:49:03 +0800

    update:修改BaseUrl

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,7 +49,7 @@ export default {
   // https://go.nuxtjs.dev/config-axios
   axios: {
     // https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://xxx.xxx.xxx/'
+    baseURL: 'https://111.230.104.252/'
   },
 
   // https://go.nuxtjs.dev/pwa
